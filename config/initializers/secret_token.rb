03ee21d3e8d966357b5d87012a4c4ed9a9612f73
AlSquire <alsquire@gmail.com>
2010-09-10 01:26:10 +0200

    rails 3 fresh

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Isocele::Application.config.secret_token = '06514ff346aaf977ccbd949b253c14a444c74642033d8de37da7ea64e9a990477401eadeb26faa2dd138dec8c8e7288b5c2cc72e497ab3d2f1901b9d50d46c24'
