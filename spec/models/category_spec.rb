8eb63fc49b18b7958836a69b7ae8b216b2eb0c40
AlSquire <alsquire@gmail.com>
2010-09-10 02:47:30 +0200

    Category model and specs

---
+++ spec/models/category_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe Category do
+  before do
+    @category = Factory(:category)
+  end
+
+  subject { @category }
+  
+  it { should respond_to(:name) }
+  its(:name) { should be_a(String) }
+  
+  it { should respond_to(:shortcut) }
+  its(:shortcut) { should be_a(String) }
+  
+  it { should respond_to(:post_threads) }
+end
---
+++ spec/support/factories.rb
@@ -0,0 +1,4 @@
+Factory.define(:category) do |c|
+  c.name "Category"
+  c.shortcut "c"
+end
