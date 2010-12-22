f6235abd2ed858dd7fa7aab41dd758cc93eb0661
AlSquire <alsquire@gmail.com>
2010-12-23 00:47:57 +0100

    PostThreadsController

--- app/controllers/post_threads_controller.rb
+++ app/controllers/post_threads_controller.rb
@@ -1,17 +1,25 @@
 class PostThreadsController < ApplicationController
   def index
-    
+    @category = Category.find_by_shortcut(params[:category_shortcut])
+    @post_threads = @category.post_threads
   end
-  
+
   def show
-    
+    @category = Category.find_by_shortcut(params[:category_shortcut])
+    @post_thread = @category.post_threads.find(params[:id])
   end
-  
+
   def create
-    redirect_to :action => :show
+    @category = Category.find_by_shortcut(params[:category_shortcut])
+    @post = Post.create(params[:post].merge(:category_id => @category.id))
+    @post_thread = @post.post_thread
+    redirect_to :action => :show, :category_shortcut => @category.shortcut, :id => @post_thread.id
   end
-  
+
   def update
-    redirect_to :action => :show
+    @category = Category.find_by_shortcut(params[:category_shortcut])
+    @post_thread = @category.post_threads.find(params[:id])
+    @post = @post_thread.posts.create(params[:post])
+    redirect_to :action => :show, :category_shortcut => @category.shortcut, :id => @post_thread.id
   end
 end
--- spec/controllers/post_threads_controller_spec.rb
+++ spec/controllers/post_threads_controller_spec.rb
@@ -1,35 +1,46 @@
 require 'spec_helper'
 
 describe PostThreadsController do
-  # "Need a matching route?"
+  let(:post_thread) { Factory.create(:post_thread, :category_id => category.id) }
+  let(:category) { Category.first }
 
   describe "GET index" do
-    it do
-      get :index, :category_shortcut => 'c'
-      # assigns(:post_threads).should ???
-      response.should be_success
-    end
+    before { get :index, :category_shortcut => category.shortcut }
+
+    it { should assign_to(:category) }
+    it { should assign_to(:post_threads) }
+    it { should respond_with(:success) }
   end
 
   describe "GET show" do
-    it do
-      get :show, :category_shortcut => 'c', :id => 'id'
-      response.should be_success
-    end
+    before { get :show, :category_shortcut => post_thread.category.shortcut, :id => post_thread.id }
+
+    it { should assign_to(:category) }
+    it { should assign_to(:post_thread) }
+    it { should respond_with(:success) }
   end
 
   describe "POST create" do
-    it do
-      post :create, :category_shortcut => 'c', :id => 'id'
-      response.should redirect_to :action => :show
-    end
+    before { post :create, :category_shortcut => category.shortcut, :post => Factory.attributes_for(:post) }
+
+    it { should assign_to(:category) }
+    it { should assign_to(:post_thread) }
+    it { should assign_to(:post) }
+    it { assigns(:post_thread).should be_persisted }
+    it { assigns(:post).should be_persisted }
+    it { should respond_with(:redirect) }
+    it { should redirect_to(:action => :show, :category_shortcut => category.shortcut, :id => assigns(:post_thread).id) }
   end
 
-  describe "PUT update" do
-    it do
-      put :update, :category_shortcut => 'c', :id => 'id'
-      response.should redirect_to :action => :show
-    end
+  describe "POST update" do
+    before { post :update, :category_shortcut => category.shortcut, :id => post_thread.id, :post => Factory.attributes_for(:post) }
+
+    it { should assign_to(:category) }
+    it { should assign_to(:post_thread) }
+    it { should assign_to(:post) }
+    it { assigns(:post).should be_persisted }
+    it { should respond_with(:redirect) }
+    it { should redirect_to(:action => :show, :category_shortcut => category.shortcut, :id => post_thread.id) }
   end
 
 end
